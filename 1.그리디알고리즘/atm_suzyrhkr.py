b031a9ae5319ee0f05b5b853457e932b962ef94b
Suzy Kwak <48341349+suzyrhkr@users.noreply.github.com>
2021-01-17 20:45:18 +0900

    Update atm_suzyrhkr.py
    두 번째 풀이 추가

--- 1.그리디알고리즘/atm_suzyrhkr.py
+++ 1.그리디알고리즘/atm_suzyrhkr.py
@@ -9,3 +9,19 @@ for i in range(n):
     sum += p[j]
 
 print(sum)
+
+--------------------------------------
+
+n = int(input())
+p = list(map(int, input().split()))
+
+p.sort() 
+
+sum = 0 
+total = 0
+
+for i in range(N): 
+    total += (sum + p[i])
+    sum += p[i] 
+        
+print(total)

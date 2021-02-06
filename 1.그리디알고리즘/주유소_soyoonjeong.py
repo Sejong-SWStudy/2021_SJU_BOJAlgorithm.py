ebbbcd5942e92f0f8a5cd3fce3970efbe34fefa4
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2021-02-06 21:45:14 +0900

    Create 주유소_soyoonjeong.py

---
+++ 1.그리디알고리즘/주유소_soyoonjeong.py
@@ -0,0 +1,11 @@
+# 13305
+n = int(input())
+road = list(map(int, input().split()))
+price = list(map(int, input().split()))
+minprice = price[0]
+result = price[0]*road[0]
+for i in range(1, n-1):
+    if minprice > price[i]:
+        minprice = price[i]
+    result += minprice*road[i]
+print(result)
